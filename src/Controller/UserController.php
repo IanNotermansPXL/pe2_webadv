6c9b0bd7c31f35d69b97f1be96b9acdcac50501a
IanNotermansPXL <ian.notermans@student.pxl.be>
2024-05-17 21:28:43 +0200

    Added loan table to pdf download

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\LoanRepository;
 use App\Repository\UserRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -40,5 +42,51 @@ class UserController extends AbstractController
         $entityManager->flush();
 
         return $this->redirectToRoute('user_loans');
+    }
+
+    #[Route("/user/loans/pdf", name: "loans_pdf")]
+    public function loansPdf(LoanRepository $loanRepository): Response
+    {
+        // Get the authenticated user
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException('The user is not logged in');
+        }
+
+        // Fetch all loans
+        $loans = $loanRepository->findBy(['user' => $user]);
+
+        // Render the loans in a Twig template
+        $html = $this->renderView('user/loans_pdf.html.twig', [
+            'user' => $user,
+            'loans' => $loans,
+        ]);
+
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("loans.pdf", [
+            "Attachment" => true
+        ]);
+
+        // Stop execution and return a response
+        return new Response('', 200, [
+            'Content-Type' => 'application/pdf',
+        ]);
     }
 }
